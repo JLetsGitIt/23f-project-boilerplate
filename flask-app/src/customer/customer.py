60ef591eaf60f069344e5bb4a69ae03bcb72ec35
LeeDylan21 <150943672+LeeDylan21@users.noreply.github.com>
2023-12-06 00:56:22 -0500

    Add files via upload

---
+++ flask-app/src/customer/customer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 19:58:41 2023
+
+@author: dylan
+"""
+
---
+++ flask-app/src/toy_manufacturer/toy_manufacturer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  6 00:52:25 2023
+
+@author: dylan
+"""
+
