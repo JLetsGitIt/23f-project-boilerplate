dc1687bd7830ae76c1b1c687bd8cf06e9ff3fa8c
tessjoseph <128314163+tessjoseph@users.noreply.github.com>
2023-12-07 14:15:23 -0500

    Update __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -36,22 +36,38 @@ def create_app():
     # Import the various Beluprint Objects
     from src.customers.customers import customers
     from src.products.products  import products
-    from src.toy.toy import toy
     from src.customization.customization  import customization
     from src.customer.customer import customer
+    from src.feedback.feedback import feedback
+    from src.gift_wrapping.gift_wrapping import gift_wrapping
+    from src.toy.toy import toy
+    from src.order.order import order
     from src.toy_manufacturer.toy_manufacturer import toy_manufacturer
-
+    from src.toy_safety_information import toy_safety_information
+    
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     toy_information = Blueprint('toy info', __name__)
     toy_information.register_blueprint(toy, url_prefix='/c')
     toy_information.register_blueprint(customization, url_prefix='/p')
+    toy_information.register_blueprint(toy_safety_information, url_prefix = '/ts')
     app.register_blueprint(toy_information, url_prefix='/t')
 
     customerandmanufacturer = Blueprint('people', __name__)
     customerandmanufacturer.register_blueprint(customer, url_prefix = '/cust')
     customerandmanufacturer.register_blueprint(toy_manufacturer, url_prefix = '/man')
     app.register_blueprint(customerandmanufacturer, url_prefix = '/cm')
+    
 
+    order_information = Blueprint('order_info', __name___)
+    order_information.register_blueprint(order, url_prefix = '/o')
+    order_information.register_blueprint(gift_wrapping, url_prefix = '/gw')
+    app.register_blueprint(order_information, url_prefix = '/oi')
+
+    feedback = Blueprint('feedback', __name__)
+    feedback.register_blueprint(feedback, url_prefix = '/f')
+    app.register_blueprint(feedback, url_prefix = '/feed')
+
+    
     # Don't forget to return the app object
     return app
