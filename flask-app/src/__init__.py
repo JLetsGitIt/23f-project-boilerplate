b3ec78e1258ddb59b56f13fff45134a53817fa08
LeeDylan21 <150943672+LeeDylan21@users.noreply.github.com>
2023-12-07 23:13:07 -0500

    Update __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -34,39 +34,37 @@ def create_app():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
     # Import the various Beluprint Objects
-    from src.customers.customers import customers
-    from src.products.products  import products
-    from src.customization.customization  import customization
+    # from src.customers.customers import customers
+    # from src.products.products  import products
+    from src.toy.toy import toy
+    # from src.customization.customization  import customization
     from src.customer.customer import customer
     from src.feedback.feedback import feedback
-    from src.gift_wrapping_service.gift_wrapping_services import gift_wrapping
-    from src.toy.toy import toy
-    from src.order.order import order
-    from src.toy_manufacturer.toy_manufacturer import toy_manufacturer
-    from src.toy_safety_information import toy_safety_information
-    
+    # from src.gift_wrapping_service.gift_wrapping_service import gift_wrapping
+    # from src.order.order import orders
+    # from src.toy_manufacturer.toy_manufacturer import toy_manufacturer
+    # from src.toy_safety_information.toy_safety_information import toy_safety
+
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
-    toy_information = Blueprint('toy info', __name__)
-    toy_information.register_blueprint(toy, url_prefix='/c')
-    toy_information.register_blueprint(customization, url_prefix='/p')
-    toy_information.register_blueprint(toy_safety_information, url_prefix = '/ts')
-    app.register_blueprint(toy_information, url_prefix='/t')
+    # toy_information = Blueprint('toy info', __name__)
 
-    customerandmanufacturer = Blueprint('people', __name__)
-    customerandmanufacturer.register_blueprint(customer, url_prefix = '/cust')
-    customerandmanufacturer.register_blueprint(toy_manufacturer, url_prefix = '/man')
-    app.register_blueprint(customerandmanufacturer, url_prefix = '/cm')
-    
+    # toy_information.register_blueprint(toy, url_prefix='/c')
+    # toy_information.register_blueprint(customization, url_prefix='/p')
+    app.register_blueprint(toy, url_prefix='/t')
+    # app.register_blueprint(customization, url_prefix = '/cust')
+    app.register_blueprint(customer, url_prefix = '/c')
+    # app.register_blueprint(feedback, url_prefix = '/f')
+    app.register_blueprint(gift_wrapping, url_prefix = '/gw')
+    app.register_blueprint(orders, url_prefix = '/o')
+    # app.register_blueprint(toy_manufacturer, url_prefix = '/tm')
+    # app.register_blueprint(toy_safety, url_prefix = '/ts')
 
-    order_information = Blueprint('order_info', __name__)
-    order_information.register_blueprint(order, url_prefix = '/o')
-    order_information.register_blueprint(gift_wrapping, url_prefix = '/gw')
-    app.register_blueprint(order_information, url_prefix = '/oi')
+    # root_password_file = '/secrets/db_root_password.txt'
+    # app.config['MYSQL_DATABASE_PASSWORD'] = open(root_password_file).readline().strip()
+    # print(f"Read MySQL root password from {root_password_file}: {app.config['MYSQL_DATABASE_PASSWORD']}")
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://webapp:MYSQL_ROOT_PASSWORD@%/toy_sellers'
 
-    feedback = Blueprint('feedback', __name__)
-    feedback.register_blueprint(feedback, url_prefix = '/f')
-    app.register_blueprint(feedback, url_prefix = '/feed')
 
     
     # Don't forget to return the app object
